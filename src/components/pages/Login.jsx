b11a8d2bea3fbc4f1e94dcf495017b73787379b3
Nacho RM <joseignaciorm13@gmail.com>
2020-02-24 16:32:13 +0100

    Creamos rutas públicas, privadas y default Page404

---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => (
+    <div className="ed-grid">
+      <h1>Página de login</h1>
+    </div>
+)
+
+export default Login
---
+++ src/components/pages/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Register = () => (
+    <div className="ed-grid">
+      <h1>Página de registro</h1>
+    </div>
+)
+
+export default Register
