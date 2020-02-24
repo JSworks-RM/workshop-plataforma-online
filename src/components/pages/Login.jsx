ac9bf3359f7d7ef8d1e798a54ed938860581a8a6
Nacho RM <joseignaciorm13@gmail.com>
2020-02-24 20:02:24 +0100

    Integramos axios para consulta método post de datos login. Usamos petición fake, guardo token en localStorage

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
+import axios from 'axios'
 
+const authentication = e => {
+  e.preventDefault()
+  const form = e.target
+
+  const data = {
+    email: form.email.value,
+    password: form.password.value
+  }
+
+  axios.post(`https://api-edteam.alejogs4.now.sh/login`, data)
+  .then(res => {
+    localStorage.setItem('token', res.data.token)
+    window.location = '/'
+  })
+  .catch(e => console.log(e) )
+}
+ 
 const Login = () => (
     <div className="ed-grid">
       <div className="l-block"></div>
       <div className="m-to-center m-60 lg-30">
         <h1 className="center">Iniciar sesión</h1>
-        <form>
+        <form onSubmit={authentication.bind()}>
           <div className="form__item">
             <label htmlFor="email">
               Correo electrónico
-              <input type="email" id="email" placeholder="Ingrese su e-mail" required />
+              <input type="email" name="email" id="email" placeholder="Ingrese su e-mail" required />
             </label>
           </div>
           <div className="form__item">
             <label htmlFor="password">
               Contraseña
-              <input type="password" id="password" placeholder="Ingrese su contraseña" required />
+              <input type="password" name="password" id="password" placeholder="Ingrese su contraseña" required />
             </label>
           </div>
           <div className="form__item">
