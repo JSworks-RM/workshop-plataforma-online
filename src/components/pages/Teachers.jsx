5c500dc238ef478a2ce7efdc30666fcf23672956
Nacho RM <joseignaciorm13@gmail.com>
2020-02-25 13:35:57 +0100

    Componente banner. Refactorizando Page Teacher y le integro nuevo componente molecules -> Banner dinámicamente

--- src/components/pages/Teachers.jsx
+++ src/components/pages/Teachers.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Banner from '../Organisms/Banner'
 
 const Teachers = () => (
-    <div className="ed-grid">
-      <h1>Profesores</h1>
-    </div>
+  <Banner 
+    color="third-color"
+    image={{
+      src: "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2017-11/banner-equipo.jpg",
+      alt: "Banner Profesores"
+    }}
+    title="Nuestros profesores"
+    subtitle="Este plan docente esta altamente calificado para guiarte en la formación"
+  />
 )
 
 export default Teachers
