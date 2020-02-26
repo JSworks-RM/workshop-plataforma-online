502b69145fc2ff1c03ea4cd3775db3e4fdea1721
Nacho RM <joseignaciorm13@gmail.com>
2020-02-26 15:01:49 +0100

    Características adicionales página curso

--- src/components/pages/Course.jsx
+++ src/components/pages/Course.jsx
@@ -30,7 +30,20 @@ const Course = ({ course }) => {
           />
           <main className="ed-grid lg-grid-10">
             <div className="lg-cols-7">
-              <div className="course-features"></div>
+              <div className="course-features ed-grid lg-grid-3 s-border s-pxy-2 s-radius s-bg-white l-block s-shadow-bottom row-gap">
+                <div>
+                  <h3 className="t4">¿Que aprenderás?</h3>
+                  <ul dangerouslySetInnerHTML={{__html: course.you_learn}} />
+                </div>
+                <div>
+                  <h3 className="t4">Conocimientos previos</h3>
+                  <ul dangerouslySetInnerHTML={{__html: course.requirements}} />
+                </div>
+                <div>
+                  <h3 className="t4">Nivel</h3>
+                  <p>{course.level}</p>
+                </div>
+              </div>
               <h2>Temario del curso</h2>
               <div className="s-border s-pxy-2 lg-pxy-4 s-radius s-bg-white l-block l-section s-shadow-bottom">
                 {
