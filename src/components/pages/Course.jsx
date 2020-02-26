384233bda67cfd67813eab1744466f3edfaed712
Nacho RM <joseignaciorm13@gmail.com>
2020-02-26 14:30:35 +0100

    Temario del curso

--- src/components/pages/Course.jsx
+++ src/components/pages/Course.jsx
@@ -28,6 +28,33 @@ const Course = ({ course }) => {
             speciality={course.data.specialities[0].name}
             info={course.information}
           />
+          <main className="ed-grid lg-grid-10">
+            <div className="lg-cols-7">
+              <div className="course-features"></div>
+              <h2>Temario del curso</h2>
+              <div className="s-border s-pxy-2 lg-pxy-4 s-radius s-bg-white l-block l-section s-shadow-bottom">
+                {
+                  course.data.classes.map(cl => (
+                      <div className="course-class l-section" key={cl.class.id}>
+                        <h3>{cl.class.title}</h3>
+                        <p>{cl.class.description}</p>
+                        <ul>
+                          {
+                            cl.subjects.map(s => (
+                            <li>{s.subject.title}</li>
+                            ))
+                          }
+                        </ul>
+                      </div>
+                  ))
+                }
+              </div>
+            </div>
+            <div className="lg-cols-3">
+              <h2 className="t3">Profesor:</h2>
+              <p>Beto Quiroga</p>
+            </div>
+          </main>
         </>
       }
     </>
